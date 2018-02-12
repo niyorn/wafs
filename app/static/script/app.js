e32d175fc16f378c771380397f73836b2d011b2a
niyorn <c.niyornram@hotmail.com>
2018-02-12 13:54:46 +0100

    assignment 1

--- app/static/script/app.js
+++ app/static/script/app.js
@@ -1,24 +1,35 @@
 //Use IIFE to create variable but only in the IIFE scope
-(function(){
-'use strict'; //use strict mode only in this scope
+(function () {
+    'use strict'; //use strict mode only in this scope
 
-var app = {
-    init = function () {
-        // do something
+    var app = {
+        init: function () {
+            routes.init();
+        }
     }
-}
 
-var route = {
-    init = function (){
-        // do something
+    var routes = {
+        init: function () {
+            window.addEventListener('hashchange', function () {
+                let hash = location.hash; //get hash without #
+                section.toggle(hash)
+            });
+        }
     }
-}
 
-var section = {
-    toggle = function(route){
-        // krijgt een parameter mee en gaat hier dus iets doen met die paramter
+    var section = {
+        toggle: function (route) {
+
+            //remove all active class
+            var section = document.querySelectorAll('section');
+            section.forEach(function (i) {
+                i.classList.remove('active');
+            });
+
+            //Add active class to the page-link that you've clicked
+            document.querySelector(route).classList.add('active');
+        }
     }
-}
 
-app.init();//start application
+    app.init(); //start application
 })();
