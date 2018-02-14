7bfa06716b9bf435a046bbea346f15f06d888eb5
niyorn <c.niyornram@hotmail.com>
2018-02-14 15:54:19 +0100

    Add 1h change

--- app/static/script/app.js
+++ app/static/script/app.js
@@ -7,11 +7,11 @@
             let xhr = new XMLHttpRequest();
             let url = "https://api.coinmarketcap.com/v1/ticker/";
 
-            xhr.open("GET", url, true);//create a async request
+            xhr.open("GET", url, true); //create a async request
             xhr.onload = function (e) {
                 if (xhr.readyState === 4) {
                     if (xhr.status === 200) {
-                        var data = JSON.parse(xhr.responseText);//Create object from the string that we get
+                        var data = JSON.parse(xhr.responseText); //Create object from the string that we get
                         routes.init(data); //init route
                     } else {
                         console.error(xhr.statusText);
@@ -54,12 +54,12 @@
         //The insert function will insert data to element with the same 'data-bind' name.
         insert: function (data) {
             let dataCoin = data.map(function (i) { //Map function thanks to Keving Wang
-                console.log(i);
                 return {
                     id: i.id,
                     rank: i.rank,
                     name: i.name,
                     price: i.price_usd,
+                    percent_change_1h: i.percent_change_1h,
                     percent_change_24h: i.percent_change_24h,
                     name_abbreviation: i.symbol
                 }
@@ -72,41 +72,53 @@
                         return "index.html/detail/" + this.id
                     }
                 },
-                percent_change_24h: {
+                percent_change_1h: {
                     class: function () {
-                        /*This function assign a class to an element if the value is higher or
-                        lower than 0*/
-                        let value = this.percent_change_24h;
-                        let className = '';
-
-                        //Check value
-                        if(value < 0){
-                            className = 'minus';
-                        }
-                        else if(value > 0){
-                            className = 'plus';
-                        }
-                        else{
-                            className = 'neutral';
-                        }
-                        
-                        return 'change '+ className //'change' is just a classt  css that add a '%' sign
+                        let value = this.percent_change_1h;
+                        let className = helper.assignClass(value); // assign class to the value
+                        return 'change ' + className //'change' is just a classt  css that add a '%' sign
                     }
                 },
-                coin_image:{
-                    src: function(){
+                percent_change_24h: {
+                    class: function () {
+                        let value = this.percent_change_24h;
+                        let className = helper.assignClass(value); // assign class to the value
+                        return 'change ' + className //'change' is just a classt  css that add a '%' sign
+                    }
+                },
+                coin_image: {
+                    src: function () {
                         let name = this.id;
-                        let url = 'https://files.coinmarketcap.com/static/img/coins/128x128/'+ name +'.png';
+                        let url = 'https://files.coinmarketcap.com/static/img/coins/128x128/' + name + '.png';
                         return url;
                     },
-                    alt: function(){
+                    alt: function () {
                         return this.id;
-                    }               
+                    }
                 }
             };
 
             //Call Transparency to inject our objects into the Dom
             Transparency.render(document.querySelector('#start div'), dataCoin, directives);
+        }
+    }
+
+    var helper = {
+        assignClass: function (value) {
+            /*This function assign a class to an element if the value is higher or
+                        lower than 0*/
+            let className = '';
+
+            //Check value
+            if (value < 0) {
+                className = 'minus';
+            } else if (value > 0) {
+                className = 'plus';
+            } else {
+                className = 'neutral';
+            }
+
+            return className;
         }
     }
 
