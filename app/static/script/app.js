ea8d21619502b2c3c55d17cc85d8831ede7fbf4f
niyorn <c.niyornram@hotmail.com>
2018-02-14 12:43:23 +0100

    Add comment to code

--- app/static/script/app.js
+++ app/static/script/app.js
@@ -6,11 +6,12 @@
         init: function () {
             let xhr = new XMLHttpRequest();
             let url = "https://api.coinmarketcap.com/v1/ticker/";
-            xhr.open("GET", url, true);
+
+            xhr.open("GET", url, true);//create a async request
             xhr.onload = function (e) {
                 if (xhr.readyState === 4) {
                     if (xhr.status === 200) {
-                        var data = JSON.parse(xhr.responseText);
+                        var data = JSON.parse(xhr.responseText);//Create object from the string that we get
                         routes.init(data); //init route
                     } else {
                         console.error(xhr.statusText);
@@ -21,6 +22,7 @@
         }
     }
 
+    //Handel the route 
     var routes = {
         init: function (data) {
             this.routie(data);
@@ -37,19 +39,19 @@
         }
     }
 
+    //This is the object Function where will manipulate the DOM elements
     var section = {
         toggle: function (route) {
-
             //remove all active class
             let section = document.querySelectorAll('section');
             section.forEach(function (i) {
                 i.classList.remove('active');
             });
-
             //Add active class to the page-link that you've clicked
             document.querySelector(route).classList.add('active');
         },
 
+        //The insert function will insert data to element with the same 'data-bind' name.
         insert: function (data) {
             let dataCoin = data.map(function (i) { //Map function thanks to Keving Wang
                 return {
@@ -61,6 +63,7 @@
                 }
             });
 
+            //directive add extra information to the element for example 'class' or 'href'
             let directives = {
                 coin_id: {
                     href: function (params) {
@@ -90,6 +93,7 @@
                 }
             };
 
+            //Call Transparency to inject our objects into the Dom
             Transparency.render(document.querySelector('#start div'), dataCoin, directives);
         }
     }
